5e5da8ac2f80d92930eb03a7b69c3d4e18f17b12
test <henrik.ormasen@sos-rasisme.no>
2013-09-24 12:57:49 +0200

    Project started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MlmmjRorAdmin::Application.config.secret_key_base = '39f76b0f32c350b8fe1f8cef61b887bf0a79c41378c41ec7ede1c0fbb8a7b5389ff1c79ef82b3a6e42d7be05ee402c4e41b50df595ebfba01a6b2f9f640f2263'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MlmmjRorAdmin::Application.config.session_store :cookie_store, key: '_mlmmj-ror-admin_session'
