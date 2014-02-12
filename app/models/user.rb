afe32e3a9ebf7a8a7ed0f82840065f75e7395d66
test <henrik.ormasen@sos-rasisme.no>
2014-02-12 18:21:00 +0100

    Kode fra Rails Tutorial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
   has_and_belongs_to_many :groups
   has_secure_password
 #  validates_presence_of :password, :on => :create
   validates_confirmation_of :password, :on => :create
   validates :password, :presence =>true,
-                    :length => { :minimum => 5, :maximum => 40 },
+                    :length => { :minimum => 7, :maximum => 40 },
                     :confirmation =>true, :on => :create
   validates :email, uniqueness: { case_sensitive: false }, email_format: { message: "doesn't look like an email address" }
   def self.tilgang(roller)
