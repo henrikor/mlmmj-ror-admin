017baf000333486fb0994f70a77bff67bf06fae6
test <henrik.ormasen@sos-rasisme.no>
2014-02-16 21:02:08 +0100

    Rettet slik at tilgang til lister nå er rett

--- app/models/liste.rb
+++ app/models/liste.rb
@@ -1,3 +1,14 @@
 class Liste < ActiveRecord::Base
 	has_and_belongs_to_many :groups
+
+	def self.lists_for_user(user)
+		lists = Array.new
+		user.groups.each do |group|
+			glist = Liste.joins(:groups).where("group_id = ?", group.id)
+	        glist.each do |list|
+	        	lists << list unless lists.include? list
+	        end
+		end
+		return lists
+	end
 end
